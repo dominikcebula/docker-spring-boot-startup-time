2f2088c9466e896534a6963f5750e5d1998bad46
Dominik Cebula <dominikcebula@gmail.com>
2025-08-10 22:48:09 +0200

    exchange-rates-sample

--- Dockerfile-layers-build-and-run.sh
+++ Dockerfile-layers-build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t exchange-rates-docker-layers -f Dockerfile-layers .
-docker run --rm -p 8080:8080 -e DB_HOST=host.docker.internal exchange-rates-docker-layers
+docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 -e DB_HOST=host.docker.internal exchange-rates-docker-layers
--- Dockerfile-simple-build-and-run.sh
+++ Dockerfile-simple-build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t exchange-rates-docker-simple -f Dockerfile-simple .
-docker run --rm -p 8080:8080 -e DB_HOST=host.docker.internal exchange-rates-docker-simple
+docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 -e DB_HOST=host.docker.internal exchange-rates-docker-simple
