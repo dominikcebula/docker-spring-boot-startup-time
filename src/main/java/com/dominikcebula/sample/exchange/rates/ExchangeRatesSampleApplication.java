be16abf043bd273f67f46225ac1eacbd38d9a0ed
Dominik Cebula <dominikcebula@gmail.com>
2025-08-10 21:18:37 +0200

    exchange-rates-sample

---
+++ src/main/java/com/dominikcebula/sample/exchange/rates/ExchangeRatesSampleApplication.java
@@ -0,0 +1,11 @@
+package com.dominikcebula.sample.exchange.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatesSampleApplication.class, args);
+    }
+}
