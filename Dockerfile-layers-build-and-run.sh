a4066dc1166d66b2df04534b352cbe332ad3ee02
Dominik Cebula <dominikcebula@gmail.com>
2025-08-11 22:11:28 +0200

    exchange-rates-sample

--- Dockerfile-layers-build-and-run.sh
+++ Dockerfile-layers-build-and-run.sh
@@ -2,4 +2,4 @@
 
 mvn clean install
 docker build -t exchange-rates-docker-layers -f Dockerfile-layers .
-docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 -e DB_HOST=host.docker.internal exchange-rates-docker-layers
+docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 --add-host=host.docker.internal:host-gateway -e DB_HOST=host.docker.internal exchange-rates-docker-layers
