ba3b58c99bebbb1fcecbcf0ba63c1a9f1bec1711
Dominik Cebula <dominikcebula@gmail.com>
2025-08-11 22:37:55 +0200

    exchange-rates-sample

--- Dockerfile-layers-cds-indexer-build-and-run.sh
+++ Dockerfile-layers-cds-indexer-build-and-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn clean install -Pwith-spring-indexer
-docker build -t exchange-rates-docker-layers-cds -f Dockerfile-layers-cds .
-docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 --add-host=host.docker.internal:host-gateway -e DB_HOST=host.docker.internal exchange-rates-docker-layers-cds
+docker build -t exchange-rates-docker-layers-cds-indexer -f Dockerfile-layers-cds .
+docker run --rm --cpus 0.5 -m 768mb -p 8080:8080 --add-host=host.docker.internal:host-gateway -e DB_HOST=host.docker.internal exchange-rates-docker-layers-cds-indexere
